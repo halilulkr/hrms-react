38e5176d69f7a4342f1e1870e60e6160c69badad
Halil ÜLKER <78586224+halilulkr@users.noreply.github.com>
2021-06-12 22:21:17 +0300

    Add files via upload

---
+++ src/layouts/Navi.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button, Icon, Menu } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
+
+export default function Navi() {
+  return (
+    <div>
+      <Menu inverted fixed="top" size="large">
+        <Container>
+          <Menu.Item name="handshake outline">
+            <Icon name="handshake outline" size="large"/>
+            HRMS
+          </Menu.Item>
+          <Menu.Item name="Home" />
+          <Menu.Item name="Job Advert" />
+          <Menu.Item name="Companies" />
+          <Menu.Menu position="right">
+
+            <Menu.Item>
+              <Button.Group>
+                <Button primary>Sign Up</Button>
+                <Button.Or />
+                <Button positive>Sign In</Button>
+              </Button.Group>
+            </Menu.Item>
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    </div>
+  );
+}
